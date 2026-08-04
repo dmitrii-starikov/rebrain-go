93b59fc5c911e9826b7bd0f1517e819a4b230a11
Dmitrii Starikov <dd.starikov@gmail.com>
2026-08-04 08:18:17 +0300

    task Abstract syntax tree

--- src/Basics-08/01_task/internal/analys/analys.go
+++ src/Basics-08/01_task/internal/analys/analys.go
@@ -1,5 +1,11 @@
 package analys
 
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+)
+
 type AnalysResult struct {
 	DeclCount    int
 	CallCount    int
@@ -8,5 +14,32 @@ type AnalysResult struct {
 }
 
 func Analys(filepath string) (*AnalysResult, error) {
-	return nil, nil
+	fileSet := token.NewFileSet()
+	node, err := parser.ParseFile(fileSet, filepath, nil, parser.ParseComments)
+	if err != nil {
+		return nil, err
+	}
+
+	result := &AnalysResult{}
+	result.ImportsCount = len(node.Imports)
+
+	ast.Inspect(node, func(n ast.Node) bool {
+		switch n := n.(type) {
+		case *ast.GenDecl:
+			// Declarations: var, const
+			if n.Tok == token.VAR || n.Tok == token.CONST {
+				result.DeclCount++
+			}
+		case *ast.AssignStmt:
+			// =, :=
+			result.AssignCount++
+		case *ast.CallExpr:
+			// f(x)
+			result.CallCount++
+		}
+
+		return true
+	})
+
+	return result, nil
 }
